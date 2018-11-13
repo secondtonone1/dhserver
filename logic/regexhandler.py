4d5a9070607ff172bf26d4b8dc24e40d853df999
secondtonone1 <secondtonone1@163.com>
2018-11-13 10:28:05 +0800

    modified:   logic/regexhandler.py

--- logic/regexhandler.py
+++ logic/regexhandler.py
@@ -8,6 +8,7 @@ import tornado.web
 
 #http://localhost:8000/?greeting='wuchen'
 class RegexHandler(tornado.web.RequestHandler):
+    '''
     def get(self,param1,param2):
         try:
             print('regex data1 is %s' %(param1) )
@@ -19,4 +20,16 @@ class RegexHandler(tornado.web.RequestHandler):
             self.write('we recive path is %s;' %(str1+param1+'/'+param2+str2))
         except:
             print('get regex argument exception')
-        
+    '''
+    def get(self,*param):
+        try:
+            print('regex data1 is %s' %(param[0]) )
+            self.write('regex data1 is %s;' %(param[0]))
+            print('regex data2 is %s' %(param[1]) )
+            self.write('regex data2 is %s;' %(param[1]))
+            str1='http://127.0.0.1:8898/test/'
+            str2='/regex'
+            self.write('we recive path is %s;' %(str1+param[0]+'/'+param[1]+str2))
+        except:
+            print('get regex argument exception')
+    post = get   
